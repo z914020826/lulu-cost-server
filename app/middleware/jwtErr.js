ba71082f36931c85a38cbd1de9c7b882b5e7777f
YiHang <914020826@qq.com>
2024-03-15 09:41:16 +0800

    feat:添加登录中间件

---
+++ app/middleware/jwtErr.js
@@ -0,0 +1,27 @@
+module.exports = secret => {
+  return async function jwtErr(ctx, next) {
+    const token = ctx.request.header.authorization;
+    let decode;
+    if (token != null && token) {
+      try {
+        decode = ctx.app.jwt.verify(token, secret);
+        await next();
+      } catch (error) {
+        console.log('error', error);
+        ctx.status = 200;
+        ctx.body = {
+          code: 401,
+          msg: 'token已过期，请重新登录',
+        };
+        return;
+      }
+    } else {
+      ctx.status = 200;
+      ctx.body = {
+        code: 401,
+        msg: '您还未登录，请先登录',
+      };
+      return;
+    }
+  };
+};
--- app/router.js
+++ app/router.js
@@ -2,8 +2,9 @@
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
-  const { router, controller } = app;
+  const { router, controller, middleware } = app;
+  const _jwt = middleware.jwtErr(app.config.jwt.secret); // 传入加密字符串
   router.post('/api/user/register', controller.user.register);
   router.post('/api/user/login', controller.user.login);
-  router.get('/api/user/test', controller.user.test);
+  router.get('/api/user/test', _jwt, controller.user.test);
 };
